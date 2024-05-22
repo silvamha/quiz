5df74fccbbabaced79fe08d9b02b28e60e30a146
Ed <silva.email@yahoo.com>
2024-05-21 19:00:09 -0500

    refactor: Separate questions and results into individual components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="ctr">
-    <questions v-if="questionsAnswered < questions.length"
-    :questions="questions"
+    <questions
+      v-if="questionsAnswered < questions.length"
+      :questions="questions"
+      :questionsAnswered="questionsAnswered"
+      @question-answered="questionAnswered"
     />
+
     <result v-else />
 
     <button type="button" class="reset-btn">Reset</button>
@@ -10,76 +14,77 @@
 </template>
 
 <script>
-import Questions from './components/Questions.vue'
-import Result from './components/Result.vue'
+import Questions from "./components/Questions.vue";
+import Result from "./components/Result.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Questions,
-    Result
+    Result,
   },
   data() {
     return {
       questionsAnswered: 0,
+      totalCorrect: 0,
       questions: [
         {
-          q: 'What is 2 + 2?',
+          q: "What is 2 + 2?",
           answers: [
             {
-              text: '4',
-              is_correct: true
+              text: "4",
+              is_correct: true,
             },
             {
-              text: '3',
-              is_correct: false
+              text: "3",
+              is_correct: false,
             },
             {
-              text: 'Fish',
-              is_correct: false
+              text: "Fish",
+              is_correct: false,
             },
             {
-              text: '5',
-              is_correct: false
-            }
-          ]
+              text: "5",
+              is_correct: false,
+            },
+          ],
         },
         {
           q: 'How many letters are in the word "Banana"?',
           answers: [
             {
-              text: '5',
-              is_correct: false
+              text: "5",
+              is_correct: false,
             },
             {
-              text: '7',
-              is_correct: false
+              text: "7",
+              is_correct: false,
             },
             {
-              text: '6',
-              is_correct: true
+              text: "6",
+              is_correct: true,
             },
             {
-              text: '12',
-              is_correct: false
-            }
-          ]
+              text: "12",
+              is_correct: false,
+            },
+          ],
         },
         {
-          q: 'Find the missing letter: C_ke',
+          q: "Find the missing letter: C_ke",
           answers: [
             {
-              text: 'e',
-              is_correct: false
+              text: "e",
+              is_correct: false,
             },
             {
-              text: 'a',
-              is_correct: true
+              text: "a",
+              is_correct: true,
             },
             {
-              text: 'i',
-              is_correct: false
-            }
-          ]
+              text: "i",
+              is_correct: false,
+            },
+          ],
         },
       ],
       results: [
@@ -87,19 +92,26 @@ export default {
           min: 0,
           max: 2,
           title: "Try again!",
-          desc: "Do a little more studying and you may succeed!"
+          desc: "Do a little more studying and you may succeed!",
         },
         {
           min: 3,
           max: 3,
           title: "Wow, you're a genius!",
-          desc: "Studying has definitely paid off for you!"
-        }
-      ]
-    }
-  }
-}
-
+          desc: "Studying has definitely paid off for you!",
+        },
+      ],
+    };
+  },
+  methods: {
+    questionAnswered(is_correct) {
+      if (is_correct) {
+        this.totalCorrect++;
+      }
+      this.questionsAnswered++;
+    },
+  },
+};
 </script>
 
 <style></style>
