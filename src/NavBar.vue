f514d2f13a252aa9bf92da3d887443b8d9d54225
Ed <silva.email@yahoo.com>
2024-05-29 05:09:24 -0500

    refactor: Add NavBar component to project

---
+++ src/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div>
+        <nav>
+            <h1>Navbar</h1>
+        </nav>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
