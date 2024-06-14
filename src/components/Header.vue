c5b4c7c16d211ef3c29b67837cb0225dab687d79
Ed <silva.email@yahoo.com>
2024-06-14 05:12:03 -0500

    refactor: Update Header.vue to include Home, About, Contact, and Login navigation items

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+<header>
+    <nav>
+        <h1>Quiz App</h1>
+        <ul><     
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+</header>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
