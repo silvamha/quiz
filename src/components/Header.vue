be9bf9f8df3842fd166c9ce84ce67c9b93519a52
Ed <silva.email@yahoo.com>
2024-06-15 05:06:58 -0500

    refactor: Add Login and Signup buttons to Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,10 @@
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
         </ul>
+        <div>
+            <button>Login</button>
+            <button>Signup</button>
+        </div>
     </nav>
 </header>
     </div>
