90dcc59ac6a1f0e7bafb96639aa67822a85f86d2
Ed <silva.email@yahoo.com>
2024-06-16 04:11:18 -0500

    refactor: Add Footer component to project

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+    <div>
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
